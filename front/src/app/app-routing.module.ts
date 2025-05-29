e8ef949beac025e42aaaabfcadc86a47bcca89b7
mabdou <abdoumelouk96@gmail.com>
2025-05-29 12:42:16 +0200

    Revert "feat(core): configure HTTP interceptors and route protection"
    This reverts commit 639aea8abfd94a04fcef2d9b22df49f368030592.

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,29 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { ProfileComponent } from './pages/profile/profile.component';
-import { AuthGuard } from './features/auth/guards/auth.guard';
 
 // consider a guard combined with canLoad / canActivate route option
 // to manage unauthenticated user to access private routes
-const routes: Routes = [
-  { path: '', component: HomeComponent },
-  {
-    path: 'auth',
-    loadChildren: () => import('./features/auth/auth.module').then(m => m.AuthModule)
-  },
-  {
-    path: 'me',
-    component: ProfileComponent,
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'topics',
-    loadChildren: () => import('./features/topics/topics.module').then(m => m.TopicsModule),
-    canActivate: [AuthGuard]
-  },
-  { path: '**', redirectTo: '' }
-];
+const routes: Routes = [{ path: '', component: HomeComponent }, { path: 'topics', loadChildren: () => import('./features/topics/topics.module').then(m => m.TopicsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -2,16 +2,13 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { AuthInterceptor } from './features/auth/interceptors/auth.interceptor';
-import { ErrorInterceptor } from './features/auth/interceptors/error.interceptor';
-import { ProfileComponent } from './pages/profile/profile.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ProfileComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -19,18 +16,7 @@ import { ProfileComponent } from './pages/profile/profile.component';
     MatButtonModule,
     HttpClientModule,
   ],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptor,
-      multi: true
-    },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: ErrorInterceptor,
-      multi: true
-    }
-  ],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
